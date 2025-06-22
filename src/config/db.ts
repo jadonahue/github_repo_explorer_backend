e17747b89a4c782fb7cf6b6925250da4723a86f6
Joshua Donahue <joshua.a.donahue@gmail.com>
2025-06-22 09:35:33 -0400

    Switch to DATABASE_URL for Heroku

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,27 @@
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: 5432,
+// });
+
+// export default pool;
+
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Heroku requires SSL
+    },
 });
 
 export default pool;
